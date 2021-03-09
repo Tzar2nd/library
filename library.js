6e4b581191d67e7515dcf6202d2e673321b94de1
Ben <ben_g_smith@msn.com>
2021-03-09 10:59:13 +0100

    update

--- library.js
+++ library.js
@@ -1,16 +1,11 @@
 let library = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, releaseDate, read) {
     this.title = title;
     this.author = author;
     this.pages = pages; 
+    this.releaseDate = releaseDate;
     this.read = read;
-    this.info = () => (
-        this.title + ", " + 
-        this.author + ", " +
-        this.pages + ", " +
-        this.read
-    );
 }
 
 function addBookToLibrary(book) {
@@ -19,17 +14,42 @@ function addBookToLibrary(book) {
 
 // Add book elements to DOM via data-key
 function addBookToDOM(book) {
+    let element = document.getElementById("card-content");
+    let div = document.createElement('div');
+    let table = document.createElement('table');
+    let text = document.createTextNode('Hi its text');
+
+    div.classList.add('card-container');
+
+    // Loop through the array and enter into the table
+    for(let i = 0; i<=library.length; i++) {
+        for(var prop in library[i]) {
+            obj = library[i];
+            Object.keys(obj).forEach((key, index) => {
+                console.log(key);
+                console.log(index);
+            });
+        }
+    }
+
+    element.appendChild(div).appendChild(table).appendChild(text);
 }
 
 // remove book from DOM via data-key
 function removeBookFromDOM(book) {
-    
 }
 
-theHobbit = new Book('The Hobbit', 'JRR Tolkien', '300', 'Yes');
-nineteenEightyFour = new Book('1984', 'George Orwell', '280', 'Yes');
+function displayBook(library) {
+    console.log(library);
+    for(const book in library) {
+        console.log(book);
+    }
+}
+
+theHobbit = new Book('The Hobbit', 'JRR Tolkien', '300', '1984', 'Yes');
+nineteenEightyFour = new Book('1984', 'George Orwell', '280', '1985', 'Yes');
 addBookToLibrary(theHobbit);
 addBookToLibrary(nineteenEightyFour);
-console.log(theHobbit.info());
-console.log(nineteenEightyFour.info());
-console.table(library);
+addBookToDOM(theHobbit);
+
+displayBook(library);
