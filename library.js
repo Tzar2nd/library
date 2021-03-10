dc0cb4f7447449599c674b1bf57285486d2cfcf7
Ben <ben_g_smith@msn.com>
2021-03-10 14:33:42 +0100

    updated read/unread toggle

--- library.js
+++ library.js
@@ -1,13 +1,17 @@
 let library = [];
 
-function Book(title, author, pages, releaseDate, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages; 
-    this.releaseDate = releaseDate;
-    this.read = read;
-    this.imageSrc = 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1546071216l/5907.jpg'
+class Book {
+    constructor(title, author, pages, releaseDate, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages; 
+        this.releaseDate = releaseDate;
+        this.read = read;
+        this.imageSrc = 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1546071216l/5907.jpg'
+    }
 }
+
+
 
 function addBookToLibrary(book) {
     library.push(book);
@@ -16,24 +20,25 @@ function addBookToLibrary(book) {
 // Add book elements to DOM via data-key
 function addBookToDOM(book, key) {
     let element = document.getElementById("card-content");
-
+    let table = document.createElement('table');
+    let tbody = document.createElement('tbody');
     let div = document.createElement('div');
+    let tr = document.createElement('tr');
+    let image = document.createElement('img');
+
     div.classList.add('card-container');
     div.setAttribute("data-key", key);
 
-    let table = document.createElement('table');
-    let tbody = document.createElement('tbody');
     table.appendChild(tbody);
-    let tr = document.createElement('tr')
     tbody.appendChild(tr);
+    element.appendChild(div).appendChild(table);
 
     // row image
-    let image = document.createElement('img');
     image.src = book.imageSrc;
     image.classList.add('book-image');
     tr.appendChild(document.createElement('td')).appendChild(image);
 
-    // Loop through the array and enter into the table
+    // Loop through the array and enter data into the table
     Object.keys(book).forEach((key) => {
         if (key != 'imageSrc') {
             td = document.createElement('td');
@@ -41,17 +46,15 @@ function addBookToDOM(book, key) {
             tr.appendChild(td);
         }
     });
-    element.appendChild(div).appendChild(table);
 
     // Read button
     button = document.createElement('button');
+    button.classList.add('status-button');
     if (book.read == 'Yes') {
         button.textContent = 'Read';
-        button.classList.add('status-button');
         button.classList.add('read-button');
     } else {
         button.textContent = 'Not Read';
-        button.classList.add('status-button');
         button.classList.add('not-read-button');
     }
     button.addEventListener('click', () => changeBookStatus(key));
@@ -67,7 +70,8 @@ function addBookToDOM(book, key) {
 }
 
 function changeBookStatus(key) {
-    book = library[key-1];
+    book = library[key];
+    console.log(key);
     const div = document.querySelector(`div[data-key="${key}"]`);
     button = div.getElementsByClassName('status-button')[0];
 
@@ -88,12 +92,23 @@ function changeBookStatus(key) {
 
 // remove book from DOM via data-key
 function removeBook(key) {
-    library[key-1] = null;
+    library[key] = null;
     console.log(library);
     const div = document.querySelector(`div[data-key="${key}"]`);
     div.textContent = '';
     div.parentNode.removeChild(div);
+    checkForEmpty();
     updateStats();
+}
+
+function checkForEmpty() {
+    if (document.getElementsByClassName('card-container')[0] === undefined) {
+        emptyMessage = document.getElementsByTagName('h4')[0];
+        emptyMessage.style.display = "block";
+    } else {
+        emptyMessage = document.getElementsByTagName('h4')[0];
+        emptyMessage.style.display = "nonegi";
+    }
 }
 
 // Modal code for popup form
@@ -149,11 +164,11 @@ theHobbit = new Book('The Hobbit', 'JRR Tolkien', '300', '1935', 'Yes');
 nineteenEightyFour = new Book('1984', 'George Orwell', '280', '1945', 'Yes');
 theRoad = new Book('The Road', 'Cormac McCarthy', '500', '1984', 'No');
 addBookToLibrary(theHobbit);
-addBookToDOM(theHobbit, library.length);
+addBookToDOM(theHobbit, library.length -1);
 addBookToLibrary(nineteenEightyFour);
-addBookToDOM(nineteenEightyFour, library.length);
+addBookToDOM(nineteenEightyFour, library.length -1);
 addBookToLibrary(theRoad);
-addBookToDOM(theRoad, library.length);
+addBookToDOM(theRoad, library.length-1);
 updateStats();
 
 const saveButton = document.querySelector('.save-button');
@@ -166,7 +181,7 @@ saveButton.addEventListener('click',
     newBook = new Book(title, author, pages, year, 'No');
 
     addBookToLibrary(newBook);
-    addBookToDOM(newBook, library.length);
+    addBookToDOM(newBook, library.length -1);
     updateStats();
 
     modal.style.display = "none";
